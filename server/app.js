c5a71a047d2d414a222c7611ffddd7df544d7411
Giratin <yassine.sta@esprit.tn>
2021-04-21 02:02:46 +0100

    fixing angular dist

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ var app = express();
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://mongo/testingdevops', { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true })
+mongoose.connect('mongodb://database/testingdevops', { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true })
   .then(()=>{
     console.log("mongodb connected");
   })
