34706834546bf50b4732184ba5d04c82979f3e2d
Giratin <yassine.sta@esprit.tn>
2021-04-29 23:04:12 +0100

    adding nginx.conf file

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: "express"
+  url: "express:4201"
 };
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: "express"
+  url: "express:4201"
 };
 
 /*
