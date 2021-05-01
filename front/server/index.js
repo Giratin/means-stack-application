ffdac29f185024491400c29103e73834ce40bde6
Giratin <yassine.sta@esprit.tn>
2021-05-01 02:15:56 +0100

    okey

---
+++ front/server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const compression = require('compression');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.static(__dirname + '/front'));
+app.use(compression());
+
+app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname));
+});
+app.all('*', (req, res) => {
+    res.status(200).sendFile('/', { root: __dirname + "/front" })
+});
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log('front on port', port);
+})
