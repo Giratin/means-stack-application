8cf5456a3d083397321310eb35044eae3c2983ad
Giratin <yassine.sta@esprit.tn>
2021-05-15 05:36:00 +0100

    fixing api prefixer url

--- server/app.js
+++ server/app.js
@@ -13,10 +13,10 @@ const mongoose = require('mongoose');
 const db_url = process.env.DB_URL || "mongodb://localhost:27017/testingdevops";
 
 mongoose.connect(db_url, { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true })
-  .then(()=>{
+  .then(() => {
     console.log("mongodb connected");
   })
-  .catch((err)=>{
+  .catch((err) => {
     console.log("mongodb error")
     console.log(err)
   })
@@ -32,8 +32,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-router.use(function(req, res, next) {
-  res.url = req.protocol + '://' + req.headers.host + process.env.API_ROUTE_PREFIX || '/'+ req.url;
+app.use(function (req, res, next) {
+  res.url = req.protocol + '://' + req.headers.host + process.env.API_ROUTE_PREFIX || '/' + req.url;
   next();
 });
 
@@ -42,12 +42,12 @@ router.use(function(req, res, next) {
 app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
